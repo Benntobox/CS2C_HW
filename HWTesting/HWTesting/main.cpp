d3059b9cfa5bfcb4243f8c2dce0b9f9052b8877e
benntoboxbenjaminsboyle3@gmail.com <benjaminsboyle3@gmail.com>
2019-09-25 23:23:31 -0700

    still testing

--- HWTesting/HWTesting/main.cpp
+++ HWTesting/HWTesting/main.cpp
@@ -10,36 +10,43 @@
 // CS 2C Foothill College
 
 #include <iostream>
-#include <string>
 using namespace std;
 
 #include "FHgraph.h"
 
-// string is the data of the vertex; int is the cost type of edges
+#include "Foothill_Sort.h"
+
+// for timing our algorithms
+
+#include <cstdlib>
+#include <time.h>
+
 // --------------- main ---------------
+//#define ARRAY_SIZE 2000
+int ARRAY_SIZE = 2000;
+
 int main()
 {
-    // build graph
-    FHgraph<string, int> myGraph1;
-    myGraph1.addEdge("v1","v2", 2);    myGraph1.addEdge("v1","v4", 1);
-    myGraph1.addEdge("v2","v4", 3);    myGraph1.addEdge("v2","v5", 10);
-    myGraph1.addEdge("v3","v1", 4);    myGraph1.addEdge("v3","v6", 5);
-    myGraph1.addEdge("v4","v3", 2);    myGraph1.addEdge("v4","v5", 2);
-    myGraph1.addEdge("v4","v6", 8);    myGraph1.addEdge("v4","v7", 4);
-    myGraph1.addEdge("v5","v7", 6);
-    myGraph1.addEdge("v7","v6", 1);
+    int k;
+    int arrayOfInts[ARRAY_SIZE];
     
-    // for debugging - only works if cout << overloaded for data type
-    myGraph1.showAdjTable();
+    for (k = 0; k < ARRAY_SIZE; k++)
+        arrayOfInts[k] = rand()%ARRAY_SIZE;
     
-    // dijkstra called from inside
-    myGraph1.showDistancesTo("v2");
-    cout << endl;
+    // how we time our algorithms -------------------------
+    clock_t startTime, stopTime;
+    startTime = clock();
     
-    myGraph1.showShortestPath("v2", "v3");
-    myGraph1.showShortestPath("v2", "v6");
-    myGraph1.showShortestPath("v2", "v7");
-    cout << endl;
+    // sort using a home made bubble sort (in Foothill_Sort.h)
+    arraySort(arrayOfInts, ARRAY_SIZE);
+    
+    // how we determine the time elapsed -------------------
+    stopTime = clock();
+    
+    // report algorithm time
+    cout << "\nAlgorithm Elapsed Time: "
+    << (double)(stopTime - startTime)/(double)CLOCKS_PER_SEC
+    << " seconds." << endl << endl;
     
     return 0;
 }
