b33e3348b7d0135b9a5b612167957647bd36a5bb
benntoboxbenjaminsboyle3@gmail.com <benjaminsboyle3@gmail.com>
2019-10-01 12:38:44 -0700

    all finished, submission ready

--- CS2C_Assignment1/CS2C_Assignment1/main.cpp
+++ CS2C_Assignment1/CS2C_Assignment1/main.cpp
@@ -6,7 +6,6 @@
 //
 
 // CS 2C Assignment #1 Part A - int Version
-
 /*
 #include <iostream>
 #include <vector>
@@ -14,126 +13,126 @@
 
 using namespace std;
 
+// ------------------------- Global Scope Definitions --------------------------
+
+bool isTargetViable(vector<int> dataSet, int TARGET)
+{
+   int total = 0;
+   for (int i = 0; i < dataSet.size(); i++)
+   {
+      total += dataSet[i];
+   }
+   if (total < TARGET)
+   {
+      return false;
+   }
+   return true;
+}
+
 // ---------------------------- Sublist Prototype ----------------------------
 
 class Sublist
 {
 public:
-    Sublist(vector<int> *orig = NULL) : sum(0), originalObjects (orig) { }
-    Sublist addItem( int indexOfItemToAdd );
-    void showSublist() const;
-    int getSum() const { return sum; }
-    
+   Sublist(vector<int> *orig = NULL) : sum(0), originalObjects (orig) { }
+   Sublist addItem( int indexOfItemToAdd );
+   void showSublist() const;
+   int getSum() const { return sum; }
+
 private:
-    int sum;
-    vector<int> *originalObjects;
-    vector<int> indices;
+   int sum;
+   vector<int> *originalObjects;
+   vector<int> indices;
 };
 
 // ------------------------ Sublist Method Definitions ------------------------
 
 Sublist Sublist::addItem(int indexOfItemToAdd) {
-    Sublist newSublist = Sublist(this->originalObjects);
-    for (int i = 0; i < this->indices.size(); i++)
-    {
-        newSublist.indices.push_back(this->indices.at(i));
-    }
-    newSublist.indices.push_back(indexOfItemToAdd);
-    newSublist.sum = this->getSum() + originalObjects->at(indexOfItemToAdd);
-    return newSublist;
+   Sublist newSublist = Sublist(this->originalObjects);
+   for (int i = 0; i < this->indices.size(); i++)
+   {
+      newSublist.indices.push_back(this->indices.at(i));
+   }
+   newSublist.indices.push_back(indexOfItemToAdd);
+   newSublist.sum = this->getSum() + originalObjects->at(indexOfItemToAdd);
+   return newSublist;
 }
 
 void Sublist::showSublist() const {
-    for (int i = 0; i < this->indices.size(); i++) {
-        int indexOfIndices = this->indices[i];
-        cout << "array[" << indexOfIndices << "] = " << originalObjects->at(indexOfIndices);
-        if (i < this->indices.size() - 1)
-        {
-            cout  << ", ";
-        }
-    }
-    cout << endl;
-}
-
-// ------------------------- Global Scope Definitions --------------------------
-
-bool isTargetViable(vector<int> dataSet, int TARGET)
-{
-    int i, total = 0;
-    for (i = 0; i < dataSet.size(); i++)
-    {
-        total += dataSet[i];
-    }
-    if (total < TARGET)
-    {
-        return false;
-    }
-    return true;
+   for (int i = 0; i < this->indices.size(); i++) {
+      int indexOfIndices = this->indices[i];
+      cout << "array[" << indexOfIndices << "] = " << originalObjects->at(indexOfIndices);
+      if (i < this->indices.size() - 1)
+      {
+         cout  << ", ";
+      }
+   }
+   cout << endl;
 }
 
 // ------------------------------------ Main ------------------------------------
 
 int main()
 {
-    int TARGET = 16;
-    vector<int> dataSet;
-    vector<Sublist> choices;
-    vector<Sublist>::iterator iter, iterBest;
-    bool foundPerfect = false;
-    
-    dataSet.push_back(20); dataSet.push_back(12); dataSet.push_back(22);
-    dataSet.push_back(15); dataSet.push_back(25);
-    dataSet.push_back(19); dataSet.push_back(29);
-    dataSet.push_back(18);
-    dataSet.push_back(11); dataSet.push_back(13); dataSet.push_back(17);
-    
-    choices.clear();
-    cout << "Target time: " << TARGET << endl;
-    
-    if (!isTargetViable(dataSet, TARGET))
-    {
-        cout << "Target too high, cannot find solution" << endl;
-        return 0;
-    }
-    
-    choices.clear();
-    choices.push_back(Sublist(&dataSet));
-    iterBest = choices.begin();
-    for (int j = 0; j < dataSet.size(); j++)
-    {
-        vector<Sublist> choicesCopyForIterating = choices;
-        for (iter = choicesCopyForIterating.begin(); iter != choicesCopyForIterating.end(); iter++)
-        {
-            if (iter->getSum() + dataSet[j] == TARGET)
-            {
-                choices.push_back(iter->addItem(j));
-                iterBest = choices.end()-1;
-                foundPerfect = true;
-                break;
-            }
-            else if (iter->getSum() + dataSet[j] < TARGET)
-            {
-                choices.push_back(iter->addItem(j));
-            }
-        }
-        if (foundPerfect)
-        {
+   int TARGET = 180;
+   vector<int> dataSet;
+   vector<Sublist> choices;
+   vector<Sublist>::iterator iter, iterBest;
+   bool foundPerfect = false;
+
+   dataSet.push_back(20); dataSet.push_back(12); dataSet.push_back(22);
+   dataSet.push_back(15); dataSet.push_back(25);
+   dataSet.push_back(19); dataSet.push_back(29);
+   dataSet.push_back(18);
+   dataSet.push_back(11); dataSet.push_back(13); dataSet.push_back(17);
+
+   choices.clear();
+   cout << "Target time: " << TARGET << endl;
+
+   if (!isTargetViable(dataSet, TARGET))
+   {
+      cout << "Target too high, cannot find solution" << endl;
+      return 0;
+   }
+
+   choices.clear();
+   choices.push_back(Sublist(&dataSet));
+   iterBest = choices.begin();
+   for (int j = 0; j < dataSet.size(); j++)
+   {
+      vector<Sublist> choicesCopyForIterating = choices;
+      for (iter = choicesCopyForIterating.begin(); iter != choicesCopyForIterating.end(); iter++)
+      {
+         if (iter->getSum() + dataSet[j] == TARGET)
+         {
+            choices.push_back(iter->addItem(j));
+            iterBest = choices.end()-1;
+            foundPerfect = true;
             break;
-        }
-    }
-    if (!foundPerfect)
-    {
-        for (iter = choices.begin(); iter != choices.end(); iter++)
-        {
-            if (TARGET - iter->getSum() < TARGET - iterBest->getSum())
-            {
-                iterBest = iter;
-            }
-        }
-    }
-    cout << "Sublist ------------------------" << endl;
-    cout << "sum: " << iterBest->getSum() << endl;
-    iterBest->showSublist();
-    return 0;
+         }
+         else if (iter->getSum() + dataSet[j] < TARGET)
+         {
+            choices.push_back(iter->addItem(j));
+         }
+      }
+      if (foundPerfect)
+      {
+         break;
+      }
+   }
+   if (!foundPerfect)
+   {
+      for (iter = choices.begin(); iter != choices.end(); iter++)
+      {
+         if (TARGET - iter->getSum() < TARGET - iterBest->getSum())
+         {
+            iterBest = iter;
+         }
+      }
+   }
+   cout << "Sublist ------------------------" << endl;
+   cout << "sum: " << iterBest->getSum() << endl;
+   iterBest->showSublist();
+   return 0;
 }
-*/
+ */
--- CS2C_Assignment1/CS2C_Assignment1/mainSecond.cpp
+++ CS2C_Assignment1/CS2C_Assignment1/mainSecond.cpp
@@ -6,169 +6,176 @@
 //  Copyright © 2019 Benjamin Boyle. All rights reserved.
 //
 
+// CS 2C Assignment #1 Part B - iTunesEntry Version
+
 #include "iTunes.h"
 #include <time.h>
-#include <iostream>
-#include <vector>
 
 using namespace std;
+
+
+// ------------------------- Global Scope Definitions --------------------------
+
+int operator+(int value, iTunesEntry entry)
+{
+   return entry.getTime() + value;
+}
+
+int operator+(iTunesEntry entry, int value)
+{
+   return entry.getTime() + value;
+}
+
+bool isTargetViable(vector<iTunesEntry> dataSet, int TARGET)
+{
+   int i, total = 0;
+   for (i = 0; i < dataSet.size(); i++)
+   {
+      total += dataSet[i].getTime();
+   }
+   if (total < TARGET)
+   {
+      return false;
+   }
+   return true;
+}
+
+ostream & operator<< (ostream &out, iTunesEntry entry)
+{
+   string currTitle = entry.getTitle();
+   currTitle.erase(remove(currTitle.begin(), currTitle.end(), '\r'), currTitle.end());
+   string currArtist = entry.getArtist();
+   currArtist.erase(remove(currArtist.begin(), currArtist.end(), '\r'), currArtist.end());
+   out << currTitle << " by " << currArtist << "(" << entry.getTime() << ") ";
+   return out;
+}
 
 // ---------------------------- Sublist Prototype ----------------------------
 
 class Sublist
 {
 public:
-    Sublist(vector<iTunesEntry> *orig = NULL) : time(0), originalObjects (orig) { }
-    Sublist addItem( int indexOfItemToAdd );
-    void showSublist() const;
-    int getTime() const { return time; }
-    
+   Sublist(vector<iTunesEntry> *orig = NULL) : time(0), originalObjects (orig) { }
+   Sublist addItem( int indexOfItemToAdd );
+   void showSublist() const;
+   int getTime() const { return time; }
+
 private:
-    int time;
-    vector<iTunesEntry> *originalObjects;
-    vector<int> indices;
+   int time;
+   vector<iTunesEntry> *originalObjects;
+   vector<int> indices;
 };
 
 // ------------------------ Sublist Method Definitions ------------------------
 
 Sublist Sublist::addItem(int indexOfItemToAdd) {
-    Sublist newSublist = Sublist(this->originalObjects);
-    for (int i = 0; i < this->indices.size(); i++)
-    {
-        newSublist.indices.push_back(this->indices.at(i));
-    }
-    newSublist.indices.push_back(indexOfItemToAdd);
-    newSublist.time = this->getTime() + originalObjects->at(indexOfItemToAdd).getTime();
-    return newSublist;
+   Sublist newSublist = Sublist(this->originalObjects);
+   for (int i = 0; i < this->indices.size(); i++)
+   {
+      newSublist.indices.push_back(this->indices.at(i));
+   }
+   newSublist.indices.push_back(indexOfItemToAdd);
+   newSublist.time = this->getTime() + originalObjects->at(indexOfItemToAdd).getTime();
+   return newSublist;
 }
 
 void Sublist::showSublist() const {
-    for (int i = 0; i < this->indices.size(); i++) {
-       int indexOfIndices = this->indices[i];
-       iTunesEntry currentSong = originalObjects->at(indexOfIndices);
-       string currTitle = currentSong.getTitle();
-       currTitle.erase(remove(currTitle.begin(), currTitle.end(), '\r'), currTitle.end());
-       cout << "array[" << indexOfIndices << "] = " << currTitle;
-       string currArtist = currentSong.getArtist();
-       currArtist.erase(remove(currArtist.begin(), currArtist.end(), '\r'), currArtist.end());
-       cout << " by " << currArtist << "(" << currentSong.getTime() << ") ";
-        if (i < this->indices.size() - 1)
-        {
-            cout  << ",  ";
-        }
-    }
-    cout << endl;
-}
-
-// ------------------------- Global Scope Definitions --------------------------
-
-int operator+(int value, iTunesEntry entry)
-{
-    return entry.getTime() + value;
-}
-
-int operator+(iTunesEntry entry, int value)
-{
-    return entry.getTime() + value;
-}
-
-bool isTargetViable(vector<iTunesEntry> dataSet, int TARGET)
-{
-    int i, total = 0;
-    for (i = 0; i < dataSet.size(); i++)
-    {
-        total += dataSet[i].getTime();
-    }
-    if (total < TARGET)
-    {
-        return false;
-    }
-    return true;
+   for (int i = 0; i < this->indices.size(); i++) {
+      int indexOfIndices = this->indices[i];
+      iTunesEntry currentSong = originalObjects->at(indexOfIndices);
+      cout << "array[" << indexOfIndices << "] = ";
+      cout << currentSong;
+      if (i < this->indices.size() - 1)
+      {
+         cout  << ",  ";
+      }
+   }
+   cout << endl;
 }
 
 // ------------------------------------ Main ------------------------------------
 
 int main()
 {
-    const int TARGET = 3600;
-    vector<iTunesEntry> dataSet;
-    vector<Sublist> choices;
-    vector<Sublist>::iterator iter, iterBest;
-    int array_size;
-    bool foundPerfect = false;
-    
-    // read the data
-    iTunesEntryReader tunes_input("itunes_file.txt");
-    if (tunes_input.readError())
-    {
-        cout << "couldn't open " << tunes_input.getFileName()
-        << " for input.\n";
-        exit(1);
-    }
-    
-    // time the algorithm -------------------------
-    clock_t startTime, stopTime;
-    startTime = clock();
-    
-    // create a vector of objects for our own use:
-    array_size = tunes_input.getNumTunes();
-    for (int k = 0; k < array_size; k++)
-        dataSet.push_back(tunes_input[k]);
-    
-    choices.clear();
-    cout << "Target time: " << TARGET << endl;
-    
-    if (!isTargetViable(dataSet, TARGET))
-    {
-        cout << "Target too high, cannot find solution" << endl;
-        return 0;
-    }
-    
-    choices.clear();
-    choices.push_back(Sublist(&dataSet));
-    iterBest = choices.begin();
-    for (int j = 0; j < dataSet.size(); j++)
-    {
-        vector<Sublist> choicesCopyForIterating = choices;
-        for (iter = choicesCopyForIterating.begin(); iter != choicesCopyForIterating.end(); iter++)
-        {
-            if (iter->getTime() + dataSet[j] == TARGET)
-            {
-                choices.push_back(iter->addItem(j));
-                iterBest = choices.end()-1;
-                foundPerfect = true;
-                break;
-            }
-            else if (iter->getTime() + dataSet[j] < TARGET)
-            {
-                choices.push_back(iter->addItem(j));
-            }
-        }
-        if (foundPerfect)
-        {
+   const int TARGET = 3600;
+   vector<iTunesEntry> dataSet;
+   vector<Sublist> choices;
+   vector<Sublist>::iterator iter, iterBest;
+   int array_size;
+   bool foundPerfect = false;
+
+   // read the data
+   iTunesEntryReader tunes_input("itunes_file.txt");
+   if (tunes_input.readError())
+   {
+      cout << "couldn't open " << tunes_input.getFileName()
+      << " for input.\n";
+      exit(1);
+   }
+
+   // time the algorithm -------------------------
+   clock_t startTime, stopTime;
+   startTime = clock();
+
+   // create a vector of objects for our own use:
+   array_size = tunes_input.getNumTunes();
+   for (int k = 0; k < array_size; k++)
+      dataSet.push_back(tunes_input[k]);
+
+   choices.clear();
+   cout << "Target time: " << TARGET << endl;
+
+   if (!isTargetViable(dataSet, TARGET))
+   {
+      cout << "Target too high, cannot find solution" << endl;
+      return 0;
+   }
+
+   choices.clear();
+   choices.push_back(Sublist(&dataSet));
+   iterBest = choices.begin();
+   for (int j = 0; j < dataSet.size(); j++)
+   {
+      vector<Sublist> choicesCopyForIterating = choices;
+      for (iter = choicesCopyForIterating.begin(); iter != choicesCopyForIterating.end(); iter++)
+      {
+         if (iter->getTime() + dataSet[j] == TARGET)
+         {
+            choices.push_back(iter->addItem(j));
+            iterBest = choices.end()-1;
+            foundPerfect = true;
             break;
-        }
-    }
-    if (!foundPerfect)
-    {
-        for (iter = choices.begin(); iter != choices.end(); iter++)
-        {
-            if (TARGET - iter->getTime() < TARGET - iterBest->getTime())
-            {
-                iterBest = iter;
-            }
-        }
-    }
-    cout << "Sublist ------------------------" << endl;
-    cout << "  sum: " << iterBest->getTime() << endl << "  ";
-    iterBest->showSublist();
-    
-    // how we determine the time elapsed -------------------
-    stopTime = clock();
-    // report algorithm time
-    cout << "\nAlgorithm Elapsed Time: "
-    << (double)(stopTime - startTime)/(double)CLOCKS_PER_SEC
-    << " seconds." << endl << endl;
-    
-    return 0;
+         }
+         else if (iter->getTime() + dataSet[j] < TARGET)
+         {
+            choices.push_back(iter->addItem(j));
+         }
+      }
+      if (foundPerfect)
+      {
+         break;
+      }
+   }
+   if (!foundPerfect)
+   {
+      for (iter = choices.begin(); iter != choices.end(); iter++)
+      {
+         if (TARGET - iter->getTime() < TARGET - iterBest->getTime())
+         {
+            iterBest = iter;
+         }
+      }
+   }
+   cout << "Sublist ------------------------" << endl;
+   cout << "  sum: " << iterBest->getTime() << endl << "  ";
+   iterBest->showSublist();
+
+   // how we determine the time elapsed -------------------
+   stopTime = clock();
+   // report algorithm time
+   cout << "\nAlgorithm Elapsed Time: "
+   << (double)(stopTime - startTime)/(double)CLOCKS_PER_SEC
+   << " seconds." << endl << endl;
+
+   return 0;
 }
