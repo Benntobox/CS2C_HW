45ece46ef5927882b7d34a0b369d55b7b50d3be0
benntoboxbenjaminsboyle3@gmail.com <benjaminsboyle3@gmail.com>
2019-10-08 17:04:24 -0700

    finished

--- CS2C_Assignment2/CS2C_Assignment2/main.cpp
+++ CS2C_Assignment2/CS2C_Assignment2/main.cpp
@@ -21,10 +21,6 @@ int main()
    // test mutators
    mat.set(2, 5, 10);
    mat.set(2, 5, 35);   // should overwrite the 10
-   mat.set(2, 0, 15);
-   mat.set(2, 4, 22);
-   mat.set(2, 8, 12);
-   mat.set(2, 3, 14);
    mat.set(3, 9, 21);
    mat.set(MAT_SIZE, 1, 5);  // should fail silently
    mat.set(9, 9, mat.get(3, 9));  // should copy the 21 here
@@ -51,4 +47,43 @@ int main()
    // show bottom right 15x15
    mat.showSubSquare(MAT_SIZE - 15, 15);
 
+   // additional given tests
+   mat.set(300, MAT_SIZE-1, 95);
+   mat.set(MAT_SIZE-1, 700, -3);
+
+   // check positions too hard to display with showSubSquare()
+   cout << "Value at 300, MAT_SIZE-1 is: " << mat.get(300, MAT_SIZE-1) << endl;
+   cout << "Value at MAT_SIZE-1, 700 is: "<< mat.get(MAT_SIZE-1, 700) << endl << endl;
+
+   // set more positions near 500, 500
+   mat.set(500, 500, 44);
+   mat.set(502, 500, 31);
+   mat.set(500, 502, 19);
+   mat.set(498, 498, -8);
+
+   // show positions near 500, 500
+   mat.showSubSquare(495, 15);
+
+   // various illegal values tested
+   if (mat.set(300, MAT_SIZE+1, 33)) { cout << "Successfully set value outside range"; }
+   if (mat.set(-1, 0, 1)) { cout << "Successfully set value outside range"; }
+   if (mat.set(0, -200, 0)) { cout << "Successfully set value outside range"; }
+
+   try
+   {
+      cout << mat.get(4, -8) << endl;
+   }
+   catch (...)
+   {
+      cout << "Out of bounds exception" << endl;
+   }
+
+   try
+   {
+      cout << mat.get(9, MAT_SIZE+1) << endl;
+   }
+   catch (...)
+   {
+      cout << "Out of bounds exception" << endl;
+   }
 }
