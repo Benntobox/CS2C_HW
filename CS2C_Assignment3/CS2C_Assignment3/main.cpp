c9db079c7d2fc3e18b34180f65283058c5c776cd
benntoboxbenjaminsboyle3@gmail.com <benjaminsboyle3@gmail.com>
2019-10-14 18:45:01 -0700

    starting to work

--- CS2C_Assignment3/CS2C_Assignment3/main.cpp
+++ CS2C_Assignment3/CS2C_Assignment3/main.cpp
@@ -10,7 +10,7 @@
 #include <cstdlib>
 using namespace std;
 
-const int MAT_SIZE = 100;
+const int MAT_SIZE = 10;
 
 typedef float* DynMat[MAT_SIZE];
 
@@ -19,12 +19,9 @@ DynMat mDyn, nDyn, ansDyn;
 float getMultValue(const DynMat & matA,  const DynMat & matB, int startColA, int startRowB, int size)
 {
    float totalValue = 0;
-   for (int col = 0; col < size; col++)
+   for (int i = 0; i < size; i++)
    {
-      for (int row = 0; row < size; row++)
-      {
-         totalValue += matA[startColA][row] * matB[col][startRowB];
-      }
+      totalValue += matA[i][startColA] * matB[startRowB][i];
    }
    return totalValue;
 }
@@ -36,8 +33,7 @@ void matMultDyn( const DynMat & matA,  const DynMat & matB, DynMat & matC, int s
    {
       for (j = 0; j < size; j++)
       {
-         float* currentRow = matC[j];
-         currentRow[k] = getMultValue(matA, matB, k, j, size);
+         matC[j][k] = getMultValue(matA, matB, k, j, size);
       }
    }
 }
@@ -48,10 +44,9 @@ void matShowDyn( const DynMat & matA, int start, int size)
    end = start + size;
    for(k = start; k < end; k++)
    {
-      const float* row = matA[k];
       for(j = start; j < end; j++)
       {
-         cout << row[j] << " ";
+         cout << matA[k][j] << " ";
       }
       cout << endl;
    }
@@ -71,8 +66,8 @@ int main()
    // allocate rows and initialize to 0
    for (r = 0; r < MAT_SIZE; r++)
    {
-      float matDynRow[MAT_SIZE];
-      float matDynAnsRow[MAT_SIZE];
+      float* matDynRow = new float[MAT_SIZE];
+      float* matDynAnsRow = new float[MAT_SIZE];
       for (c = 0; c < MAT_SIZE; c++)
       {
          matDynRow[c] = 0;
@@ -84,12 +79,12 @@ int main()
 
    // generate small% (bet .1 and 10%) non-default values (bet 0 and 1)
    smallPercent = MAT_SIZE/20. * MAT_SIZE;  // div by 20. means 5%, of course
-   for (r = 0; r < smallPercent; r++)
+   for (r = 0; r < smallPercent*10; r++)
    {
-      randFrac = rand() % 1000;
+      randFrac = (rand() % 10);
       randCol = rand() % MAT_SIZE;
       randRow = rand() % MAT_SIZE;
-      matDyn[randRow][randCol] = randFrac;
+      matDyn[randRow][randCol] = randFrac - 3;
    }
 
    // 10x10 submatrix in lower right
